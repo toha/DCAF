ca69e62c209fcdf63c97f21ed4eb9997acdfdfa4
Tobias Hartwich <mail@tha.io>
2015-05-08 16:19:23 +0200

    CAM added a few more common functions (url and output specific)

--- cam/src/common.c
+++ cam/src/common.c
@@ -44,3 +44,60 @@ int write_file(char *file_name, char *output_txt) {
   fclose(outputFile);
   return 0;
 }
+
+int parse_uri(char *uri_str, UriUriA *uri) {
+  UriParserStateA state;
+  state.uri = uri;
+  if (uriParseUriA(&state, uri_str) != URI_SUCCESS) {
+    /* Failure */
+    printf("Failure\n");
+    uriFreeUriMembersA(uri);
+    return -1;
+  }
+  // printf("pathHead: %s\n", (*uri).pathHead->text.first);
+  return 0;
+}
+
+void hexDump(char *desc, void *addr, int len) {
+  int i;
+  unsigned char buff[17];
+  unsigned char *pc = (unsigned char *)addr;
+
+  // Output description if given.
+  if (desc != NULL)
+    printf("%s:\n", desc);
+
+  // Process every byte in the data.
+  for (i = 0; i < len; i++) {
+    // Multiple of 16 means new line (with line offset).
+
+    if ((i % 16) == 0) {
+      // Just don't print ASCII for the zeroth line.
+      // if (i != 0)
+      //    printf ("  %s\n", buff);
+
+      // Output the offset.
+      // printf ("  %04x ", i);
+    }
+
+    // Now the hex code for the specific character.
+    printf(" %02x", pc[i]);
+
+    // And store a printable ASCII character for later.
+    if ((pc[i] < 0x20) || (pc[i] > 0x7e))
+      buff[i % 16] = '.';
+    else
+      buff[i % 16] = pc[i];
+    buff[(i % 16) + 1] = '\0';
+  }
+
+  // Pad out last line if not exactly 16 characters.
+  while ((i % 16) != 0) {
+    printf("   ");
+    i++;
+  }
+
+  // And print the final ASCII bit.
+  // printf ("  %s\n", buff);
+  printf("\n");
+}
