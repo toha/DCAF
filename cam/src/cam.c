ad1530fcaad85e1596346a53a23a7011f2690a6c
Tobias Hartwich <mail@tha.io>
2015-05-09 11:47:12 +0200

    added simple coap-app to CAM

--- cam/src/cam.c
+++ cam/src/cam.c
@@ -7,10 +7,43 @@
 #define COAP_MEDIATYPE_APPLICATION_DCAF 70
 #define CBOR_MAX_STR 255
 
+int init_resource(coap_context_t *);
+
+struct MemoryStruct {
+  char *memory;
+  size_t size;
+};
+
+// Callback for libcurl for getting response from memory
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb,
+                                  void *userp) {
+  size_t realsize = size * nmemb;
+  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+  if (mem->memory == NULL) {
+    /* out of memory! */
+    printf("not enough memory\n");
+    return 0;
+  }
+
+  memcpy(&(mem->memory[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+
+  return realsize;
+}
 
 int main(int argc, char **argv) {
   printf("---------------------------------------------------\n");
   printf("Client Authorization Manager (CAM) - pid: %d\n", getpid());
   printf("---------------------------------------------------\n");
 
+  coap_application_t *app;
+  coap_endpoint_t *interface;
+  coap_address_t listen_addr;
+  int result = EXIT_FAILURE;
+  coap_set_log_level(LOG_DEBUG);
+
+  app = coap_new_application();
 }
--- cam/src/cam.h
+++ cam/src/cam.h
@@ -1,6 +1,7 @@
 #ifndef _DCAF_CAM_H_
 #define _DCAF_CAM_H_
 
+#include "common.h"
 #include <curl/curl.h>
 #include "lib/riot-cbor/cbor.h"
 
