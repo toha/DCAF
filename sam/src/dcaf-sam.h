67e7ab11e37f36f408b8c622be1e99d81918ceef
Tobias Hartwich <mail@tha.io>
2015-06-09 14:12:09 +0200

    added common functions to SAM

--- sam/src/dcaf-sam.h
+++ sam/src/dcaf-sam.h
@@ -1,4 +1,15 @@
-#ifndef _DCAFSAM_H_
-#define _DCAFSAM_H_
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "lib/sys/queue.h"
+#include <sys/time.h>
+#include <uriparser/Uri.h>
+#include "lib/mongoose/mongoose.h"
+
+void print_bits(size_t const size, void const *const ptr);
 
 #endif
