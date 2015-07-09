43ce0cbdaecd7358b49b3c32c35d63c2b4a43ad1
Tobias Hartwich <mail@tha.io>
2015-07-09 12:15:01 +0200

    init revocation thread and lock with mutex

--- sam/src/revocation.c
+++ sam/src/revocation.c
@@ -18,3 +18,32 @@ void my_coap_response_handler(struct coap_context_t *ctx,
                               coap_pdu_t *received, const coap_tid_t id) {
   // Nothing to do async cause coap-client is working synchronously (internally)
 }
+
+
+int revocation_run() {
+  LIST_HEAD(all_revocations_list, dcaf_revocation) revocation_list;
+  LIST_INIT(&revocation_list);
+  if (0 != dao_get_revocations(&revocation_list)) {
+    return MG_FALSE;
+  }
+  struct dcaf_revocation *np;
+  LIST_FOREACH(np, &revocation_list, next) {
+    // skip delivered revocations
+
+    pthread_mutex_lock(&dao_mutex);
+    if (0 != dao_edit_revocation(np->ticket.id, np)) {
+      pthread_mutex_unlock(&dao_mutex);
+      printf("Update revocation error!\n");
+      return 1;
+    }
+    pthread_mutex_unlock(&dao_mutex);
+  }
+  return 0;
+}
+
+int init_revocation_thread() {
+  while (1) {
+    revocation_run();
+    sleep(1);
+  }
+}
