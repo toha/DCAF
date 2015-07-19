84e92a38d111c54515e6eebffc74e70badf9036f
Tobias Hartwich <mail@tha.io>
2015-07-19 14:53:32 +0200

    added failure checks to commissioning

--- sam/src/commissioning.c
+++ sam/src/commissioning.c
@@ -17,4 +17,34 @@ void coap_comissioning_hndl(struct coap_context_t *ctx,
 
 int try_send_commissioning(json_t *commissioning_msg) {
   printf("Send commissioning\n");
+
+
+  json_t *j_rs_uri = json_object_get(commissioning_msg, "server_uri");
+  json_t *j_ticket_json = json_object_get(commissioning_msg, "ticket");
+  json_t *j_new_key = json_object_get(commissioning_msg, "new_key");
+  json_t *j_new_sam = json_object_get(commissioning_msg, "new_sam");
+
+  if (!j_rs_uri || !j_ticket_json || !j_new_sam || !j_new_key ||
+      !json_is_string(j_rs_uri) || !json_is_object(j_ticket_json) ||
+      !json_is_string(j_new_key) || !json_is_string(j_new_sam)) {
+    return -1;
+  }
+
+  char *rs_uri = json_string_value(j_rs_uri);
+  char *new_sam = json_string_value(j_new_sam);
+  char *new_key_b64 = json_string_value(j_new_key);
+  size_t new_key_size = 0;
+  unsigned char *new_key =
+      base64_decode(new_key_b64, strlen(new_key_b64), &new_key_size);
+
+  if (0 >= new_key_size) {
+    printf("Invalid B64 Key\n");
+    return -2;
+  }
+
+  struct dcaf_ticket commissioning_ticket;
+  if (0 != json2ticket(j_ticket_json, &commissioning_ticket)) {
+    printf("Error on json2ticket\n");
+    return -3;
+  }
 }
