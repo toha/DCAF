b949658d2ce331f8e4ebce33109121156a321cae
Tobias Hartwich <mail@tha.io>
2015-08-30 19:23:47 +0200

    DCAF client implemented communication with constrained resource server and response handler

--- client/dcaf-client.c
+++ client/dcaf-client.c
@@ -96,7 +96,6 @@ static struct etimer et2;
 #define TOGGLE_INTERVAL 30
 #define TOGGLE_INTERVAL2 5
 
-
 static coap_context_t *dcaf_coap_context;
 static struct uip_udp_conn *dcaf_dtls_conn;
 static dtls_context_t *dcaf_dtls_context;
@@ -170,6 +169,315 @@ static int get_psk_key(struct dtls_context_t *ctx, const session_t *sess,
     *result = &server_psk;
   }
   return 0;
+}
+
+static ssize_t dcaf_network_send(struct coap_context_t *c,
+                                 const coap_endpoint_t *local_interface,
+                                 const coap_address_t *dst, unsigned char *data,
+                                 size_t datalen) {
+#ifndef NDEBUG
+  printf("call: dcaf_network_send\n");
+#endif
+  uint16_t local_port = uip_htons(local_interface->addr.port);
+  uint16_t remote_port = uip_htons(dst->port);
+
+  if (COAP_DEFAULT_PORT == remote_port) {
+    return coap_network_send(c, local_interface, dst, data, datalen);
+  } else {
+    session_t ss;
+    dtls_session_init(&ss);
+    uip_ipaddr_copy(&ss.addr, &dst->addr);
+    ss.ifindex = local_interface->ifindex;
+    ss.port = dst->port;
+    ss.size = sizeof(ss.addr) + sizeof(ss.port);
+
+    return dtls_write(dcaf_dtls_context, &ss, (uint8 *)data, datalen);
+  }
+}
+
+static int read_from_peer(struct dtls_context_t *ctx, session_t *session,
+                          uint8 *data, size_t len) {
+#ifndef NDEBUG
+  printf("call: read_from_peer\n");
+#endif
+
+  coap_packet_t *packet_state;
+  packet_state = (coap_packet_t *)custom_coap_malloc_packet();
+  coap_address_init(&(packet_state)->dst); /* the local interface address */
+  coap_address_init(&(packet_state)->src); /* the remote peer */
+
+  uip_ipaddr_copy(&packet_state->src.addr, &UIP_IP_BUF->srcipaddr);
+  packet_state->src.port = UIP_UDP_BUF->srcport;
+  uip_ipaddr_copy(&packet_state->dst.addr, &UIP_IP_BUF->destipaddr);
+  packet_state->dst.port = UIP_UDP_BUF->destport;
+
+  packet_state->length = len;
+  memcpy(&packet_state->payload, data, len);
+
+  int a = coap_handle_message(dcaf_coap_context, dcaf_coap_context->endpoint,
+                              packet_state);
+
+  coap_free_packet(packet_state);
+
+  return a;
+}
+
+static int send_to_peer(struct dtls_context_t *ctx, session_t *session,
+                        uint8 *data, size_t len) {
+
+  struct uip_udp_conn *conn = (struct uip_udp_conn *)dtls_get_app_data(ctx);
+
+  uip_ipaddr_copy(&conn->ripaddr, &session->addr);
+  conn->rport = session->port;
+
+#ifndef NDEBUG
+  PRINTF("send to ");
+  PRINT6ADDR(&conn->ripaddr);
+  PRINTF(":%u\n", uip_ntohs(conn->rport));
+#endif
+
+  uip_udp_packet_send(conn, data, len);
+
+  /* Restore server connection to allow data from any node */
+  memset(&conn->ripaddr, 0, sizeof(conn->ripaddr));
+  memset(&conn->rport, 0, sizeof(conn->rport));
+
+  return len;
+}
+
+static void dtls_handle_read(dtls_context_t *ctx) {
+#ifndef NDEBUG
+  printf("call: dtls_handle_read\n");
+#endif
+
+  static session_t session;
+  uip_ipaddr_copy(&session.addr, &UIP_IP_BUF->srcipaddr);
+  session.port = UIP_UDP_BUF->srcport;
+  session.size = sizeof(session.addr) + sizeof(session.port);
+
+  ((char *)uip_appdata)[uip_datalen()] = 0;
+#ifndef NDEBUG
+  PRINTF("client received message from ");
+  PRINT6ADDR(&session.addr);
+  PRINTF(":%d\n", uip_ntohs(session.port));
+#endif
+
+  dtls_handle_message(ctx, &session, uip_appdata, uip_datalen());
+}
+
+static void handle_tcpip_event() {
+
+#ifndef NDEBUG
+  printf("call: handle_tcpip_event\n");
+#endif
+
+  if (uip_newdata()) {
+    uint16_t srcport = uip_htons(UIP_UDP_BUF->srcport);
+    if (COAP_DEFAULT_PORT == srcport) {
+      coap_read(dcaf_coap_context);
+    } else {
+      dtls_handle_read(dcaf_dtls_context);
+    }
+  }
+}
+
+static inline void coap_message_handler(struct coap_context_t *ctx,
+                                        const coap_endpoint_t *local_interface,
+                                        const coap_address_t *remote,
+                                        coap_pdu_t *sent, coap_pdu_t *received,
+                                        const coap_tid_t id) {
+  msg_state.hnd(ctx, local_interface, remote, sent, received, id);
+}
+
+static int dtls_event(struct dtls_context_t *ctx, session_t *sess,
+                      dtls_alert_level_t level, unsigned short code);
+
+static dtls_handler_t dtls_cb = {
+    .write = send_to_peer,
+    .read = read_from_peer,
+    .event = dtls_event,
+    .get_psk_key = get_psk_key,
+};
+
+static void init_network() {
+
+#ifndef NDEBUG
+  printf("call: init_network\n");
+#endif
+
+  uip_ipaddr_t ipaddr;
+
+  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0x200, 0, 0, 0);
+  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+
+  uip_ip6addr(&cam_dst.addr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0x1);
+  cam_dst.port = uip_htons(COAPS_DEFAULT_PORT);
+  cam_dst.size = sizeof(cam_dst.addr) + sizeof(cam_dst.port);
+
+  uip_ip6addr(&server_dst.addr, 0xaaaa, 0, 0, 0, 0x200, 0, 0, 0x2);
+  server_dst.port = uip_htons(COAPS_DEFAULT_PORT);
+  server_dst.size = sizeof(server_dst.addr) + sizeof(server_dst.port);
+
+  uip_ip6addr(&server_dst_nocrypt.addr, 0xaaaa, 0, 0, 0, 0x200, 0, 0, 0x2);
+  server_dst_nocrypt.port = uip_htons(COAP_DEFAULT_PORT);
+  server_dst_nocrypt.size =
+      sizeof(server_dst_nocrypt.addr) + sizeof(server_dst_nocrypt.port);
+
+  // dtls connection and context. first for cam
+  dcaf_dtls_conn = udp_new(&cam_dst.addr, 0, NULL);
+  udp_bind(dcaf_dtls_conn, cam_dst.port);
+
+  dtls_set_log_level(DTLS_LOG_DEBUG);
+
+  dcaf_dtls_context = dtls_new_context(dcaf_dtls_conn);
+  if (dcaf_dtls_context) {
+    dtls_set_handler(dcaf_dtls_context, &dtls_cb);
+  }
+
+  coap_address_t ca;
+  coap_address_init(&ca);
+  uip_ipaddr_copy(&ca.addr, &ipaddr);
+  ca.port = uip_htons(20221);
+  ca.size = sizeof(ca.addr) + sizeof(ca.port);
+
+  dcaf_coap_context = coap_new_context(&ca);
+  dcaf_coap_context->network_send = dcaf_network_send;
+
+  coap_register_response_handler(dcaf_coap_context, coap_message_handler);
+}
+
+static int create_pdu(coap_context_t *ctx, char *uri, size_t uri_length,
+                      method_t method, coap_pdu_t **pdu) {
+
+  size_t segmentbufsize = 255;
+  unsigned char segmentbuf[segmentbufsize];
+  unsigned char *segmentbufptr = segmentbuf;
+
+  coap_uri_t curi;
+
+  if (!(*pdu = coap_new_pdu())) {
+    return -1;
+  }
+
+  (*pdu)->hdr->type = COAP_MESSAGE_CON;
+  (*pdu)->hdr->id = coap_new_message_id(ctx);
+  (*pdu)->hdr->code = method;
+
+  (*pdu)->hdr->token_length = the_token.length;
+  if (!coap_add_token(*pdu, the_token.length, the_token.s)) {
+    return -2;
+  }
+
+  if (0 != coap_split_uri(uri, uri_length, &curi)) {
+    return -3;
+  }
+
+  int split_path_res = coap_split_path(curi.path.s, curi.path.length,
+                                       segmentbuf, &segmentbufsize);
+  if (0 > split_path_res) {
+    return -4;
+  }
+
+  while (split_path_res--) {
+    coap_add_option((*pdu), COAP_OPTION_URI_PATH,
+                    COAP_OPT_LENGTH(segmentbufptr),
+                    COAP_OPT_VALUE(segmentbufptr));
+    segmentbufptr += COAP_OPT_SIZE(segmentbufptr);
+  }
+
+  unsigned char buf[3];
+  coap_add_option((*pdu), COAP_OPTION_CONTENT_TYPE,
+                  coap_encode_var_bytes(buf, COAP_MEDIATYPE_APPLICATION_DCAF),
+                  buf);
+
+  return 0;
+}
+
+static void
+create_coap_message(void (*hnd)(struct coap_context_t *ctx,
+                                const coap_endpoint_t *local_interface,
+                                const coap_address_t *remote, coap_pdu_t *sent,
+                                coap_pdu_t *received, const coap_tid_t id),
+                    char *uri, size_t uri_length, method_t method,
+                    coap_pdu_t **pdu) {
+  create_pdu(dcaf_coap_context, uri, uri_length, method, pdu);
+
+  msg_state.msg_id = (*pdu)->hdr->id;
+  msg_state.hnd = hnd;
+}
+
+static void connect_server() {
+  dcaf_dtls_conn = udp_new(&server_dst.addr, 0, NULL);
+  dcaf_dtls_context->app = dcaf_dtls_conn;
+  dtls_connect(dcaf_dtls_context, &server_dst);
+}
+
+static void handle_cam_response(struct coap_context_t *ctx,
+                                const coap_endpoint_t *local_interface,
+                                const coap_address_t *remote, coap_pdu_t *sent,
+                                coap_pdu_t *received, const coap_tid_t id) {
+#ifdef DCAF_TIME
+  server_last.cpu = energest_type_time(ENERGEST_TYPE_CPU);
+#endif
+
+  size_t payload_len;
+  unsigned char *payload;
+
+  if (!coap_get_data(received, &payload_len, &payload) || 0 >= payload_len) {
+    return;
+  }
+
+  const cn_cbor *camcb;
+  camcb = cn_cbor_decode((char *)payload, payload_len, NULL);
+
+  if (!camcb || camcb->type != CN_CBOR_MAP) {
+#ifndef NDEBUG
+    printf("no cbor ticket found\n");
+#endif
+    return;
+  }
+
+#if !defined(NDEBUG) || defined(DCAF_DEBUG)
+  printf("C<-CAM: got ticket from cam\n");
+#endif
+
+#ifndef NDEBUG
+  printf("payload (%d Byte):\n", payload_len);
+#endif
+
+  const cn_cbor *f = cn_cbor_mapget_int(camcb, DCAF_TYPE_F);
+  const cn_cbor *v = cn_cbor_mapget_int(camcb, DCAF_TYPE_V);
+  if (!f || !v) {
+    return;
+  }
+
+  dcaf_ticket_face_length =
+      cbor_encoder_write(dcaf_ticket_face, 0, DCAF_MAX_FACE, f);
+
+  dcaf_ticket_verifier_length = v->length;
+  memcpy(&dcaf_ticket_verifier, v->v.str, v->length);
+
+#ifndef NDEBUG
+  printf("ticket face (%d Byte):\n", dcaf_ticket_face_length);
+  hexdump(dcaf_ticket_face, dcaf_ticket_face_length);
+  printf("verifier (%d Byte):\n", dcaf_ticket_face_length);
+  hexdump(dcaf_ticket_verifier, dcaf_ticket_verifier_length);
+#endif
+#ifdef DCAF_TIME
+  server_diff.cpu = energest_type_time(ENERGEST_TYPE_CPU) - server_last.cpu;
+  printf("Time: Handle CAM response (coap + create face): %li\n",
+         server_diff.cpu);
+#endif
+
+  // Got a Ticket. Start DTLS-Handshake with Server
+  dtls_close(dcaf_dtls_context, &cam_dst);
+  dtls_free_context(dcaf_dtls_context);
+
+#if !defined(NDEBUG) || defined(DCAF_DEBUG)
+  printf("C->S: start dtls handshake\n");
+#endif
+  connect_server();
 }
 
 static void connect_cam() { dtls_connect(dcaf_dtls_context, &cam_dst); }
@@ -295,7 +603,6 @@ static void send_cam_request() {
   printf("Time create and send cam request: %li\n", server_diff.cpu);
 #endif
 }
-
 
 static void send_authorized_resource_request() {
 #ifdef DCAF_TIME
